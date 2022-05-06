57223abbe588c848742a75e65fa7e175b3577deb
Artur Rocha <arturrocha1.2@outlook.com>
2022-05-06 15:40:40 -0300

    master

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-var id = document.querySelector('#input_nome').value;
-var link = document.createElementNS("cases2", "a");
-    link.href = `cases2/${id}`;
-    link.target = '_blank';
-    var event = new MouseEvent('click', {
-        'view': window,
-        'bubbles': false,
-        'cancelable': true
-    });
-    link.dispatchEvent(event);
+function cases() {
+    var id = document.querySelector('#input_nome').value;
+    window.location.replace(`cases2/${id}`, '_blank');
+}
