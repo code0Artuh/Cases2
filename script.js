d715a70a41fa20cc1cbdc5b581ae107e47a59bd4
Artur Rocha <arturrocha1.2@outlook.com>
2022-05-06 16:12:35 -0300

    master

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function cases() {
-    var id = document.querySelector('#input_nome').value;
-    window.location.replace(`cases2/${id}`, '_blank');
-}
+document.querySelector("#case").addEventListener("click", function() {
+var id = document.querySelector('#input_nome').value
+window.location.replace = "cases2/"+id;
+})
